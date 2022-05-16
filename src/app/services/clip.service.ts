9398ae5cba3459f9d48a309c1a151d5625c65e9f
Ali Qazipoor Qoomati <qazipoor@gmail.com>
2022-05-16 19:29:43 +0300

    Querying the Database

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -5,6 +5,9 @@ import {
   DocumentReference
 } from '@angular/fire/compat/firestore'
 import IClip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +17,28 @@ export class ClipService {
 
   constructor(
     private db: AngularFirestore,
+    private auth: AngularFireAuth
   ) {
     this.clipsCollection = db.collection('clips')
   }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data)
+  }
+
+  getUserClips() {
+    return this.auth.user.pipe(
+      switchMap(user => {
+        if (!user) {
+          return of([])
+        }
+
+        const query = this.clipsCollection.ref.where(
+          'uid', '==', user.uid
+        )
+
+        return query.get()
+      })
+    )
   }
 }
