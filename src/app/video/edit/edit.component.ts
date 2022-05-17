5d2d9b7fc89b17ce14c98a644d2b46c46c27eda0
Ali Qazipoor Qoomati <qazipoor@gmail.com>
2022-05-17 14:26:40 +0300

    Updating Clips

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-edit',
@@ -11,6 +11,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null;
+  inSubmission = false
+  showAlert = false
+  alertColor = 'blue'
+  alertMessage = 'Please wait! Updating clip.'
 
   clipID = new FormControl('');
   title = new FormControl('',
@@ -25,7 +29,10 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
     id: this.clipID
   });
 
-  constructor(private modal: ModalService) {}
+  constructor(
+    private modal: ModalService,
+    private clipService: ClipService
+  ) { }
 
   ngOnInit(): void {
     this.modal.register('editClip');
@@ -42,5 +49,27 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
 
     this.clipID.setValue(this.activeClip.docID)
     this.title.setValue(this.activeClip.title)
+  }
+
+  async submit() {
+    this.inSubmission = true
+    this.showAlert = true
+    this.alertColor = 'blue'
+    this.alertMessage = 'Please wait! Updating clip.'
+
+    try {
+      await this.clipService.updateClip(
+        this.clipID.value, this.title.value
+      )
+    } catch(e) {
+      this.inSubmission = false
+      this.alertColor = 'red'
+      this.alertMessage = 'Something went wrong. Try again later'
+      return
+    }
+
+    this.inSubmission = false
+    this.alertColor = 'green'
+    this.alertMessage = 'Success!'
   }
 }
