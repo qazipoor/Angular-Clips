43aee452be42752b8e233a78b4337ec2542a0fa9
Ali Qazipoor Qoomati <qazipoor@gmail.com>
2022-05-19 01:31:08 +0300

    Saving Files in Memory

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
-import { createFFmpeg } from '@ffmpeg/ffmpeg';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FfmpegService {
-  isReady = false
+  isReady = false;
   private ffmpeg;
 
   constructor() {
-    this.ffmpeg = createFFmpeg({ log: true })
+    this.ffmpeg = createFFmpeg({ log: true });
   }
 
   async init() {
     if (this.isReady) {
-      return
+      return;
     }
 
-    await this.ffmpeg.load()
+    await this.ffmpeg.load();
 
-    this.isReady = true
+    this.isReady = true;
+  }
+
+  async getScreenshots(file: File) {
+    const data = await fetchFile(file);
+
+    this.ffmpeg.FS('writeFile', file.name, data);
   }
 }
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -48,7 +48,7 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel();
   }
 
-  storeFile($event: Event) {
+  async storeFile($event: Event) {
     this.isDragover = false;
 
     this.file = ($event as DragEvent).dataTransfer
@@ -58,6 +58,8 @@ export class UploadComponent implements OnDestroy {
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
+
+    await this.ffmpegService.getScreenshots(this.file);
 
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
