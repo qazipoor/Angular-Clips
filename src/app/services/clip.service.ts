40e7d8225a9729ea96102cabb3e0fcb4dbdc0fb0
Ali Qazipoor Qoomati <qazipoor@gmail.com>
2022-05-19 17:03:16 +0300

    Querying for Clips

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -15,7 +15,9 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
   providedIn: 'root',
 })
 export class ClipService {
-  public clipsCollection: AngularFirestoreCollection<IClip>;
+  public clipsCollection: AngularFirestoreCollection<IClip>
+  pageClips: IClip[] = []
+  pendingReq = false
 
   constructor(
     private db: AngularFirestore,
@@ -70,5 +72,39 @@ export class ClipService {
     await screenshotRef.delete();
 
     await this.clipsCollection.doc(clip.docID).delete()
+  }
+
+  async getClips() {
+    if (this.pendingReq) {
+      return
+    }
+
+    this.pendingReq = false
+
+    let query = this.clipsCollection.ref.orderBy(
+      'timestamp',
+      'desc'
+    ).limit(6)
+
+    const { length } = this.pageClips
+
+    if (length) {
+      const lastDocID = this.pageClips[length - 1].docID
+      const lastDoc = await this.clipsCollection.doc(lastDocID)
+        .get().toPromise()
+      
+      query = query.startAfter(lastDoc)
+    }
+
+    const snapshot = await query.get()
+
+    snapshot.forEach(doc => {
+      this.pageClips.push({
+        docID: doc.id,
+        ...doc.data(),
+      });
+    })
+
+    this.pendingReq = false;
   }
 }
