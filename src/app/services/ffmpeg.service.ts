43e3a9de490a224433e77732c1687959e067df01
Ali Qazipoor Qoomati <qazipoor@gmail.com>
2022-05-19 13:48:06 +0300

    Uploading a Blob

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -71,4 +71,11 @@ export class FfmpegService {
 
     return screenshots
   }
+
+  async blobFromURL(url: string) {
+    const response = await fetch(url)
+    const blob = await response.blob()
+
+    return blob
+  }
 }
