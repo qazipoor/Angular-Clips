2af78f72d029312826dada9a16762e6e8fd5e8d7
Ali Qazipoor Qoomati <qazipoor@gmail.com>
2022-05-19 21:31:49 +0300

    Fixing the Timestamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ import { AboutComponent } from './about/about.component';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { ClipsListComponent } from './clips-list/clips-list.component'
+import { ClipsListComponent } from './clips-list/clips-list.component';
+import { FbTimestampPipe } from './pipes/fb-timestamp.pipe'
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { ClipsListComponent } from './clips-list/clips-list.component'
     ClipComponent,
     NotFoundComponent,
     ClipsListComponent,
+    FbTimestampPipe,
   ],
   imports: [
     BrowserModule,
